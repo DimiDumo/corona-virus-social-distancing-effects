56ffb07aad94d4d58a3e79460a73a8ff1270c25b
Dimitri Dumonet <dimitri@emax-digital.com>
2020-03-22 16:17:12 +0100

    Updates

---
+++ precache-manifest.27bdd4cfefc066f9d7eb663c3daef86d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dc6b2ca88701a00652d1730f47440a89",
+    "url": "/corona-virus-social-distancing-effect/index.html"
+  },
+  {
+    "revision": "0a872372e8a4815cd566",
+    "url": "/corona-virus-social-distancing-effect/static/css/2.4b6e3ef2.chunk.css"
+  },
+  {
+    "revision": "0a2aa7a99aa426b3090f",
+    "url": "/corona-virus-social-distancing-effect/static/css/main.2d51ff4f.chunk.css"
+  },
+  {
+    "revision": "0a872372e8a4815cd566",
+    "url": "/corona-virus-social-distancing-effect/static/js/2.88a9c5d2.chunk.js"
+  },
+  {
+    "revision": "117d65892edc8adcd29d4a0d57b5a80a",
+    "url": "/corona-virus-social-distancing-effect/static/js/2.88a9c5d2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0a2aa7a99aa426b3090f",
+    "url": "/corona-virus-social-distancing-effect/static/js/main.356823bf.chunk.js"
+  },
+  {
+    "revision": "fa5880a6fed679e50b06",
+    "url": "/corona-virus-social-distancing-effect/static/js/runtime-main.ba3f64a3.js"
+  }
+]);
